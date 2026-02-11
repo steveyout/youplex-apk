e16e70c2f040732a1acdf1f9df11cbc38368f1b9
steveyout <youtsteve1@gmail.com>
2026-02-11 15:12:43 +0300

    fixed minor bugs

--- plugins/withPawnsSDK.js
+++ plugins/withPawnsSDK.js
@@ -1,7 +1,32 @@
-const { withAppBuildGradle, withProjectBuildGradle } = require('@expo/config-plugins');
+const { withAndroidManifest, withAppBuildGradle, withProjectBuildGradle } = require('@expo/config-plugins');
 
 module.exports = function withPawnsSDK(config) {
-    // Add Maven Repo
+    // 1. Inject the Service into AndroidManifest.xml
+    config = withAndroidManifest(config, (config) => {
+        const mainApplication = config.modResults.manifest.application[0];
+
+        // We must manually add the service tag that the Pawns SDK uses
+        if (!mainApplication.service) mainApplication.service = [];
+
+        // Check if the service is already there to avoid duplicates
+        const hasService = mainApplication.service.some(
+            (s) => s.$['android:name'] === 'app.pawns.sdk.common.service.PawnsService'
+        );
+
+        if (!hasService) {
+            mainApplication.service.push({
+                $: {
+                    'android:name': 'app.pawns.sdk.common.service.PawnsService',
+                    'android:enabled': 'true',
+                    'android:exported': 'false',
+                    'android:foregroundServiceType': 'dataSync', // CRITICAL FOR ANDROID 14
+                },
+            });
+        }
+        return config;
+    });
+
+    // 2. Add Maven Repo to Project build.gradle
     config = withProjectBuildGradle(config, (config) => {
         if (!config.modResults.contents.includes('https://maven.pawns.app')) {
             config.modResults.contents = config.modResults.contents.replace(
@@ -12,7 +37,7 @@ module.exports = function withPawnsSDK(config) {
         return config;
     });
 
-    // Add implementation
+    // 3. Add Implementation to App build.gradle
     return withAppBuildGradle(config, (config) => {
         const pawnsDep = `dependencies { \n    implementation "app.pawns:android-pawns-sdk:1.7.0"`;
         if (!config.modResults.contents.includes('android-pawns-sdk')) {
